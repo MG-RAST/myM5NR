c8b9b8e7002fd77c1f11c14f87fb7a9e6aae2e3f
Wolfgang Gerlach <mail@wolfgang-gerlach.com>
2015-04-30 15:37:59 -0500

    solr5

--- solr/run_solr.sh
+++ solr/run_solr.sh
@@ -2,26 +2,33 @@
 
 # this script runs solr in foreground
 
-# max Java memory is 80% of system memory
+
 
 set -e
 set -x
 
-NAME="solr"
-PIDFILE="/var/run/${NAME}.pid"
-SOLR_DIR="/kb/runtime/solr/example"
-TOT_MEM=`free -m | awk '/Mem:/ { print $2 }'`
-JAVA_MEM=`echo "${TOT_MEM} * 0.8" | bc | cut -f1 -d"."`
-JAVA_OPTIONS="-Xms1024M -Xmx${JAVA_MEM}M -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -jar start.jar"
-JAVA=`which java`
+export PIDFILE="/var/run/solr.pid"
+export SOLR_INCLUDE="/opt/solr/bin/solr.in.sh"
+
+/opt/solr/bin/solr -f start
+
+# max Java memory is 80% of system memory
+
+#NAME="solr"
+#PIDFILE="/var/run/${NAME}.pid"
+#SOLR_DIR="/kb/runtime/solr/example"
+#TOT_MEM=`free -m | awk '/Mem:/ { print $2 }'`
+#JAVA_MEM=`echo "${TOT_MEM} * 0.8" | bc | cut -f1 -d"."`
+#JAVA_OPTIONS="-Xms1024M -Xmx${JAVA_MEM}M -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -jar start.jar"
+#JAVA=`which java`
 
 
-echo -n "Starting $NAME... "
-if [ -f $PIDFILE ]; then
-	echo "is already running!"
-else
-	cd $SOLR_DIR
-	$JAVA $JAVA_OPTIONS
-	echo "(Done)"
-fi
+#echo -n "Starting $NAME... "
+#if [ -f $PIDFILE ]; then
+#	echo "is already running!"
+#else
+#	cd $SOLR_DIR
+#	$JAVA $JAVA_OPTIONS
+#	echo "(Done)"
+#fi
 
