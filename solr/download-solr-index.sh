d9cc41475e03e859aa2ead27007fd9560313b3be
Jared Bischof <jbischof@gmail.com>
2015-05-11 20:55:59 +0000

    Editing download and setup scripts to handle a list of m5nr versions to download and setup.  Make v1 and v10 the defaults.

--- solr/download-solr-index.sh
+++ solr/download-solr-index.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 # set a default values
-M5NR_VERSION="10"
+M5NR_VERSIONS="1 10"
 SOLR_VERSION="5.0.0"
 TARGET="/mnt"
-
-set -e
-set -x
 
 # binary location from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 BIN=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -18,33 +15,36 @@ if [ ! -e ${DEP_CONFIG} ]; then
     exit 1
 fi
 
+set -e
+set -x
 source ${DEP_CONFIG}
 
-echo ""
-echo "M5NR_VERSION = $M5NR_VERSION"
-echo "SOLR_VERSION = $SOLR_VERSION"
-echo "TARGET = $TARGET"
-echo ""
+for i in $M5NR_VERSIONS
+do
+    M5NR_VERSION=$i
+    echo ""
+    echo "SOLR_VERSION = $SOLR_VERSION"
+    echo "TARGET = $TARGET"
+    echo "M5NR_VERSION = $M5NR_VERSION"
+    echo ""
+    URL=''
+    if [ "$M5NR_VERSION" == "10" ] && [ "$SOLR_VERSION" == "5.0.0" ]; then
+        URL="http://shock.metagenomics.anl.gov/node/bd7bdbf9-dfba-4794-89e3-6f1bd0b5b9a8?download"
+    elif [ "$M5NR_VERSION" == "1" ] && [ "$SOLR_VERSION" == "5.0.0" ]; then
+        URL="http://shock.metagenomics.anl.gov/node/442aa062-6684-4cbd-ab1b-33b4d1ec5de6?download"
+    fi  
 
+    if [ "$URL" == "" ]; then
+        echo "Data index not found for this solr/m5nr version.";
+        exit 1;
+    fi
 
-URL=''
-#if [[ $M5NR_VERSION -eq '10' ]] && [[ $SOLR_VERSION -eq '5.0.0' ]]; then
-if [ "$M5NR_VERSION" == "10" ] && [ "$SOLR_VERSION" == "5.0.0" ]; then
-    URL="http://shock.metagenomics.anl.gov/node/bd7bdbf9-dfba-4794-89e3-6f1bd0b5b9a8?download"
-elif [ "$M5NR_VERSION" == "1" ] && [ "$SOLR_VERSION" == "5.0.0" ]; then
-    URL="http://shock.metagenomics.anl.gov/node/442aa062-6684-4cbd-ab1b-33b4d1ec5de6?download"
-fi  
+    echo "URL = $URL";
 
-if [ "$URL" == "" ]; then
-    echo "Data index not found for this solr/m5nr version.";
-    exit 1;
-fi
-
-echo "URL = $URL";
-
-export INDEX_DIR=${TARGET}/m5nr_${M5NR_VERSION}/data/index/
-if [ ! -d ${INDEX_DIR} ]; then
-  mkdir -p ${INDEX_DIR}
-  curl -s "${URL}" | tar -zxvf - -C ${INDEX_DIR}
-fi
+    export INDEX_DIR=${TARGET}/m5nr_${M5NR_VERSION}/data/index/
+    if [ ! -d ${INDEX_DIR} ]; then
+        mkdir -p ${INDEX_DIR}
+        curl -s "${URL}" | tar -zxvf - -C ${INDEX_DIR}
+    fi
+done
 exit 0;
--- solr/setup-m5nr-core.sh
+++ solr/setup-m5nr-core.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set a default values
-M5NR_VERSION=10
+M5NR_VERSIONS="1 10"
 
 # binary location from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 BIN=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -14,16 +14,20 @@ if [ ! -e ${DEP_CONFIG} ]; then
 fi
 
 source ${DEP_CONFIG}
-
-echo ""
-echo "M5NR_VERSION = $M5NR_VERSION"
-echo ""
-
 set -e
 set -x
 
-cp -av /opt/solr/server/solr/configsets/sample_techproducts_configs /opt/solr/server/solr/m5nr_${M5NR_VERSION}
-echo "name=m5nr_${M5NR_VERSION}" > /opt/solr/server/solr/m5nr_${M5NR_VERSION}/core.properties
-cp schema.xml /opt/solr/server/solr/m5nr_${M5NR_VERSION}/conf/schema.xml
-cp solr.in.sh /opt/solr/bin
-tpage --define data_dir=/mnt/data --define max_bool=100000 solrconfig.xml.tt > /opt/solr/server/solr/m5nr_${M5NR_VERSION}/conf/solrconfig.xml
+for i in $M5NR_VERSIONS
+do
+    M5NR_VERSION=$i
+    echo ""
+    echo "M5NR_VERSION = $M5NR_VERSION"
+    echo ""
+
+    cp -av /opt/solr/server/solr/configsets/sample_techproducts_configs /opt/solr/server/solr/m5nr_${M5NR_VERSION}
+    echo "name=m5nr_${M5NR_VERSION}" > /opt/solr/server/solr/m5nr_${M5NR_VERSION}/core.properties
+    cp schema.xml /opt/solr/server/solr/m5nr_${M5NR_VERSION}/conf/schema.xml
+    cp solr.in.sh /opt/solr/bin
+    tpage --define data_dir=/mnt/m5nr_${M5NR_VERSION}/data --define max_bool=100000 solrconfig.xml.tt > /opt/solr/server/solr/m5nr_${M5NR_VERSION}/conf/solrconfig.xml
+done
+exit 0;
