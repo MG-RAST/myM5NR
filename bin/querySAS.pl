ea5586b77caf4bc5c9d63125190ef03858d1261b
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-06-17 12:01:47 -0500

    fix

--- bin/querySAS.pl
+++ bin/querySAS.pl
@@ -96,14 +96,18 @@ foreach my $source (@sources){
 				if ($@) {
 					print STDERR "Processing Subsystem $ss failed [$current/$total]\n";
 					$success = 0;
+					sleep 10;
+					
 				};
+			}
+			
 				# file written sucessfully, rename it:
 				if ( $success == 1 ) {
 					rename($ss_filename_part, $ss_filename);
 				} else {
 					die $@;
 				}
-			}
+			
 		}
 		#TODO: loop to merge files ?
 
