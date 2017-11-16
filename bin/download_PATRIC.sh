93857566fdb934a97dd6af73263ab6b1ca90790d
Folker Meyer <folker@anl.gov>
2017-11-16 13:01:02 -0600

    changed PATRIC download script to use faa files changed config

--- bin/download_PATRIC.sh
+++ bin/download_PATRIC.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for file in $(curl --silent ftp://ftp.patricbrc.org/patric2/current_release/gbf/  | grep -o "[0-9\.]*.PATRIC.gbf" ) ; do
+for file in $(curl --silent ftp://ftp.patricbrc.org/patric2/current_release/faa/  | grep -o "[0-9\.]*.PATRIC.faa" ) ; do
   if [ ! -e ${file} ] ; then
     rm -f ${file}_part
-    curl --silent -o ${file}_part ftp://ftp.patricbrc.org/patric2/current_release/gbf/${file} && mv ${file}_part ${file}
+    curl --silent -o ${file}_part ftp://ftp.patricbrc.org/patric2/current_release/faa/${file} && mv ${file}_part ${file}
   fi 
 done
