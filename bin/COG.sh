2c69eae90a98fac2cb123b7aea5b06baeffa6b33
Folker Meyer <folker@anl.gov>
2017-11-21 12:42:36 -0600

    fixed separator char

--- bin/COG.sh
+++ bin/COG.sh
@@ -2,28 +2,28 @@
 
 # print the COG functional category mapping
 cat << EOF
-INFORMATION STORAGE AND PROCESSING\tTranslation, ribosomal structure and biogenesis\tJ
-INFORMATION STORAGE AND PROCESSING\tRNA processing and modification\tA
-INFORMATION STORAGE AND PROCESSING\tTranscription\tK
-INFORMATION STORAGE AND PROCESSING\tReplication, recombination and repair\tL
-INFORMATION STORAGE AND PROCESSING\tChromatin structure and dynamics\tB
-CELLULAR PROCESSES AND SIGNALING\tCell cycle control, cell division, chromosome partitioning\tD
-CELLULAR PROCESSES AND SIGNALING\tNuclear structure\tY
-CELLULAR PROCESSES AND SIGNALING\tDefense mechanisms\tV
-CELLULAR PROCESSES AND SIGNALING\tSignal transduction mechanisms\tT
-CELLULAR PROCESSES AND SIGNALING\tCell wall/membrane/envelope biogenesis\tM
-CELLULAR PROCESSES AND SIGNALING\tCell motility\tN
-CELLULAR PROCESSES AND SIGNALING\tCytoskeleton\tZ
-CELLULAR PROCESSES AND SIGNALING\tExtracellular structures\tW
-CELLULAR PROCESSES AND SIGNALING\tIntracellular trafficking, secretion, and vesicular transport\tU
-CELLULAR PROCESSES AND SIGNALING\tPosttranslational modification, protein turnover, chaperones\tO
-METABOLISM\tEnergy production and conversion\tC
-METABOLISM\tCarbohydrate transport and metabolism\tG
-METABOLISM\tAmino acid transport and metabolism\tE
-METABOLISM\tNucleotide transport and metabolism\tF
-METABOLISM\tCoenzyme transport and metabolism\tH
-METABOLISM\tLipid transport and metabolism\tI
-METABOLISM\tInorganic ion transport and metabolism\tP
-METABOLISM\tSecondary metabolites biosynthesis, transport and catabolism\tQ
-POORLY CHARACTERIZED\tGeneral function prediction only\tR
-POORLY CHARACTERIZED\tFunction unknown\tS
+INFORMATION STORAGE AND PROCESSING|Translation, ribosomal structure and biogenesis|J
+INFORMATION STORAGE AND PROCESSING|RNA processing and modification|A
+INFORMATION STORAGE AND PROCESSING|Transcription|K
+INFORMATION STORAGE AND PROCESSING|Replication, recombination and repair|L
+INFORMATION STORAGE AND PROCESSING|Chromatin structure and dynamics|B
+CELLULAR PROCESSES AND SIGNALING|Cell cycle control, cell division, chromosome partitioning|D
+CELLULAR PROCESSES AND SIGNALING|Nuclear structure|Y
+CELLULAR PROCESSES AND SIGNALING|Defense mechanisms|V
+CELLULAR PROCESSES AND SIGNALING|Signal transduction mechanisms|T
+CELLULAR PROCESSES AND SIGNALING|Cell wall/membrane/envelope biogenesis|M
+CELLULAR PROCESSES AND SIGNALING|Cell motility|N
+CELLULAR PROCESSES AND SIGNALING|Cytoskeleton|Z
+CELLULAR PROCESSES AND SIGNALING|Extracellular structures|W
+CELLULAR PROCESSES AND SIGNALING|Intracellular trafficking, secretion, and vesicular transport|U
+CELLULAR PROCESSES AND SIGNALING|Posttranslational modification, protein turnover, chaperones|O
+METABOLISM|Energy production and conversion|C
+METABOLISM|Carbohydrate transport and metabolism|G
+METABOLISM|Amino acid transport and metabolism|E
+METABOLISM|Nucleotide transport and metabolism|F
+METABOLISM|Coenzyme transport and metabolism|H
+METABOLISM|Lipid transport and metabolism|I
+METABOLISM|Inorganic ion transport and metabolism|P
+METABOLISM|Secondary metabolites biosynthesis, transport and catabolism|Q
+POORLY CHARACTERIZED|General function prediction only|R
+POORLY CHARACTERIZED|Function unknown|S
