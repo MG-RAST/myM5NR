a889ca147f313996f632c13be49d4c70748a70e9
Folker Meyer <folker@anl.gov>
2017-11-22 11:04:51 -0600

    CAZy reviewed

--- bin/create-cazy-hierarchy.sh
+++ bin/create-cazy-hierarchy.sh
@@ -1,11 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
 cat << EOF
 # name, short desc, long desc
-GH,Glycoside Hydrolases,hydrolysis and/or rearrangement of glycosidic bonds 
-GT,GlycosylTransferases,formation of glycosidic bonds
-PL,Polysaccharide Lyases,non-hydrolytic cleavage of glycosidic bonds
-CE,Carbohydrate Esterases,hydrolysis of carbohydrate esters
-AA,Auxiliary Activities,redox enzymes that act in conjunction with CAZymes
-CBM,Carbohydrate-Binding Modules,adhesion to carbohydrates
+GH,Glycoside Hydrolases[hydrolysis and/or rearrangement of glycosidic bonds]
+GT,GlycosylTransferases[formation of glycosidic bonds]
+PL,Polysaccharide Lyases[non-hydrolytic cleavage of glycosidic bonds]
+CE,Carbohydrate Esterases[hydrolysis of carbohydrate esters]
+AA,Auxiliary Activities[redox enzymes that act in conjunction with CAZymes]
+CBM,Carbohydrate-Binding Modules[adhesion to carbohydrates]
 EOF
