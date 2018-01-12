22e0805145610dd43807051330691d10c7f446c6
Travis Harrison <guumby@gmail.com>
2018-01-12 11:42:07 -0600

    remove taxonomy

--- bin/card.pl
+++ bin/card.pl
@@ -2,7 +2,7 @@
 
 # card
 #
-# extract md52id, md52seq. md52func, id2func, md52tax, id2tax
+# extract md52id, md52hier, md52seq, md52func
 # header format:
 # >gb|AFJ59957.1|ARO:3001989|CTX-M-130 [Escherichia coli]
 # >gb|AEX08599.1|ARO:3002356|NDM-6 [Escherichia coli]
@@ -24,34 +24,22 @@ use IO::Uncompress::Gunzip;
 # the main trick is to read the document record by record
 
 my $filename = shift @ARGV;
-my $taxonomy = shift @ARGV;
 
-unless ( $filename && $taxonomy ) {
-    print STDERR "Usage: \tcard.pl <filename> <taxonomy>\n";
-    print STDERR " \te.g. card.pl card.fasta ncbi_taxonomy.csv\n";
+unless ($filename) {
+    print STDERR "Usage: \tcard.pl <filename>\n";
+    print STDERR " \te.g. card.pl card.fasta\n";
     exit 1;
 }
 
 my $fh1 = new IO::Uncompress::Gunzip("$filename")
   or die "Cannot open '$filename': $!\n";
 
-open( my $ncbitax, '<', $taxonomy ) or die "cannot open $taxonomy";
-
 open( my $md52id,   '>', 'md52id.txt' )   or die;
 open( my $md52hier, '>', 'md52hier.txt' ) or die;
 open( my $md52seq,  '>', 'md52seq.txt' )  or die;
-open( my $md52tax,  '>', 'md52tax.txt' )  or die;
 open( my $md52func, '>', 'md52func.txt' ) or die;
 
-## generate a hash with NCBI taxnomy string to ID mapping
-my %ncbihash = ();
-while ( my $line = <$ncbitax> ) {
-    my ( $id, $taxstring, $junk ) = ( split /,/, $line );
-    $taxstring =~ s/"//g;
-    $ncbihash{$taxstring} = $id;
-}
-
-my ( $id, $md5s, $func, $tax, $card, $seq );
+my ( $id, $md5s, $func, $card, $seq );
 
 while (<$fh1>) {
 
@@ -76,8 +64,6 @@ while (<$fh1>) {
         $card = $fields[2];
         my @parts = split(/ \[/, $fields[3]);
         $func = $parts[0];
-        $tax  = $parts[1];
-        $tax =~ s/\]//;
     }
     else {
         s/\s+//g;    # remove whitespace
@@ -99,14 +85,13 @@ sub process_record {
     $md5s = md5_hex($seq);
 
     # print the output
-    if ( $id && $func && $tax ) {
+    if ( $id && $func && $card ) {
         print $md52id "$md5s\t$id\n";
         print $md52seq "$md5s\t$seq\n";
         print $md52func "$md5s\t$func\n";
         print $md52hier "$md5s\t$card\n";
-        print $md52tax "$md5s\t$ncbihash{$tax}\n";
     }
 
     # reset the values for the next record
-    ( $id, $md5s, $func, $tax, $card, $seq ) = ( '', '', '', '', '', '' );
+    ( $id, $md5s, $func, $card, $seq ) = ( '', '', '', '', '' );
 }
