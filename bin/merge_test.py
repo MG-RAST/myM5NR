8ec7f4b061db721ba5b65a3f1cc4c634f7c5a455
Travis Harrison <guumby@gmail.com>
2018-02-12 11:04:07 -0600

    more tests

--- bin/merge_test.py
+++ bin/merge_test.py
@@ -31,13 +31,12 @@ md5 : {
 
 def main(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--output", dest="output", default=None, help="output file with key list")
+    parser.add_argument("-k", "--keys", dest="keys", default=None, help="input file with key list, optional")
+    parser.add_argument("-o", "--output", dest="output", default=None, help="output file with key list, optional")
     parser.add_argument("-d", "--db", dest="db", default=None, help="DB path")
     parser.add_argument("--dbtype", dest="dbtype", default=None, help="DB type, one of: berkeleyDB or levelDB")
     args = parser.parse_args()
     
-    if not args.output:
-        parser.error("missing output")
     if (args.dbtype != 'berkeleyDB') and (args.dbtype != 'levelDB'):
         parser.error("invalid DB type")
     if (args.dbtype == 'levelDB') and (not os.path.isdir(args.db)):
@@ -57,27 +56,43 @@ def main(args):
         sys.stderr.write("unable to open DB at %s\n"%(args.db))
         return 1
     
-    ohdl = open(args.output, 'w')
+    ohdl = open(args.output, 'w') if args.output else None
     
     print "start reading %s: %s"%(args.db, str(datetime.now()))
     count = 0;
-    if IsLevelDB:
-        for key, value in db:
-            ohdl.write(key+"\n")
-            count += 1
+    
+    if args.keys and os.path.isfile(args.keys):
+        ihdl = open(args.keys, 'r')
+        for key in ihdl:
+            if IsLevelDB:
+                value = db.get(key)
+                if value:
+                    count += 1
+            else:
+                if db.has_key(key):
+                    count += 1
+        ihdl.close()
     else:
-        while True:
-            try:
-                (key, value) = db.next()
-                ohdl.write(key+"\n")
+        if IsLevelDB:
+            for key, value in db:
+                if ohdl:
+                    ohdl.write(key+"\n")
                 count += 1
-            except:
-                break
+        else:
+            while True:
+                try:
+                    (key, value) = db.next()
+                    if ohdl:
+                        ohdl.write(key+"\n")
+                    count += 1
+                except:
+                    break
     
     print "done reading: "+str(datetime.now())
     print "found %d keys"%(count)
     db.close()
-    ohdl.close()
+    if ohdl:
+        ohdl.close()
     
     return 0
 
