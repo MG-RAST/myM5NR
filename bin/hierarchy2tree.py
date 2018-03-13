aaf754a20b3288e86910b6af484bd2cda74798f0
Travis Harrison <guumby@gmail.com>
2018-03-13 11:32:20 -0500

    output formats

--- bin/hierarchy2tree.py
+++ bin/hierarchy2tree.py
@@ -11,7 +11,7 @@ from nested_dict import nested_dict
 FUNC_HIER_FILE = 'id2hierarchy.txt'
 
 def buildTree(fname, skipdash=False):
-    tree = nested_dict()
+    aTree = nested_dict()
     hdl  = open(fname, 'r')
     for i, line in enumerate(hdl):
         cols = line.strip().split('\t')
@@ -20,20 +20,38 @@ def buildTree(fname, skipdash=False):
             cols = filter(lambda x: x != '-', cols)
         else:
             cols = map(lambda x: 'null' if x == '-' else x, cols)
-        if cols == 0:
+        if len(cols) == 0:
             continue
         branch = {'id': cid, 'depth': len(cols)}
         for c in reversed(cols):
             branch = {c: branch}
-        tree.update(nested_dict(branch))
+        aTree.update(nested_dict(branch))
     hdl.close()
-    return tree.to_dict(), i
+    return aTree.to_dict(), i
+
+def buildMap(fname, skipdash=False):
+    aMap = {}
+    hdl  = open(fname, 'r')
+    for i, line in enumerate(hdl):
+        cols = line.strip().split('\t')
+        cid = cols.pop(0)
+        if skipdash:
+            cols = filter(lambda x: x != '-', cols)
+        else:
+            cols = map(lambda x: None if x == '-' else x, cols)
+        if len(cols) == 0:
+            continue
+        aMap[cid] = cols
+    hdl.close()
+    return aMap, i
+        
 
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", dest="input", default=None, help="input file, depends on type")
     parser.add_argument("--type", dest="type", default=None, help="hierarchy type to parse: taxonomy or functional")
-    parser.add_argument("--output", dest="output", default=None, help="output file name")
+    parser.add_argument("--output", dest="output", default=None, help="output file prefix, will create <output>.<format>.json")
+    parser.add_argument("--format", dest="format", default='both', help="output format, one of: 'tree', 'map', 'both'")
     parser.add_argument("--parsedir", dest="parsedir", default="../", help="Directory containing parsed source dirs")
     args = parser.parse_args()
     
@@ -47,11 +65,15 @@ def main(args):
         parser.error("invalid dir for parsed source dirs")
     
     hierTree = {}
+    hierMaP  = {}
     
     # parse just a single file
     if args.type == 'taxonomy':
         print "start reading %s: %s"%(args.input, str(datetime.now()))
-        hierTree, count = buildTree(args.input, True)
+        if (args.format == 'both') or (args.format == 'tree'):
+            hierTree, count = buildTree(args.input, True)
+        if (args.format == 'both') or (args.format == 'map'):
+            hierMaP, count = buildMap(args.input, True)
         print "done reading: "+str(datetime.now())
         print "processed %d taxa"%(count)
         
@@ -69,15 +91,22 @@ def main(args):
         for source in fhSrcs:
             print "start reading %s: %s"%(source, str(datetime.now()))
             fname = os.path.join(args.parsedir, source, FUNC_HIER_FILE)
-            subTree, count = buildTree(fname, False)
-            hierTree[source] = subTree
+            if (args.format == 'both') or (args.format == 'tree'):
+                subTree, count = buildTree(fname, False)
+                hierTree[source] = subTree
+            if (args.format == 'both') or (args.format == 'map'):
+                subMap, count = buildMap(fname, False)
+                hierMaP[source] = subMap
             print "done reading: "+str(datetime.now())
-            print "processed %d brances for %s"%(count, source)
+            print "processed %d branches for %s"%(count, source)
         
     else:
         parser.error("invalid hierarchy type")
     
-    json.dump(hierTree, open(args.output, 'w'))
+    if hierTree:
+        json.dump(hierTree, open(args.output+'.tree.json', 'w'))
+    if hierMaP:
+        json.dump(hierMaP, open(args.output+'.map.json', 'w'))
     
     return 0
 
