b444c38f918f071780189542b3272020832cedc9
Travis Harrison <guumby@gmail.com>
2018-03-16 11:26:09 -0500

    more control

--- bin/m5nr_parse.sh
+++ bin/m5nr_parse.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TODO=$1
+
 FIRST_LIST="BacMet CARD GenBank NCBI-Taxonomy SEED-Subsystems motuDB"
 SECOND_LIST="Greengenes PATRIC PhAnToMe RDP RefSeq SEED-Annotations SILVA-LSU SILVA-SSU Swiss-Prot TrEmble"
 THIRD_LIST="goslim CAZy COG EC EggNOG InterPro KEGG PFAM"
@@ -8,13 +10,19 @@ PROCS=4
 
 cd /m5nr_data
 
-echo "Parsing ${FIRST_LIST}"
-echo ${FIRST_LIST} | tr ' ' '\n' | xargs -n 1 -I {} -P ${PROCS} /myM5NR/bin/m5nr_compiler.py parse -d -f -s {}
+if [ "$TODO" -eq "1" ] || [ "$TODO" -eq "all" ]; then
+    echo "Parsing ${FIRST_LIST}"
+    echo ${FIRST_LIST} | tr ' ' '\n' | xargs -n 1 -I {} -P ${PROCS} /myM5NR/bin/m5nr_compiler.py parse -d -f -s {}
+fi
 
-echo "Parsing ${SECOND_LIST}"
-echo ${SECOND_LIST} | tr ' ' '\n' | xargs -n 1 -I {} -P ${PROCS} /myM5NR/bin/m5nr_compiler.py parse -d -f -s {}
+if [ "$TODO" -eq "2" ] || [ "$TODO" -eq "all" ]; then
+    echo "Parsing ${SECOND_LIST}"
+    echo ${SECOND_LIST} | tr ' ' '\n' | xargs -n 1 -I {} -P ${PROCS} /myM5NR/bin/m5nr_compiler.py parse -d -f -s {}
+fi
 
-echo "Parsing ${THIRD_LIST}"
-echo ${THIRD_LIST} | tr ' ' '\n' | xargs -n 1 -I {} -P ${PROCS} /myM5NR/bin/m5nr_compiler.py parse -d -f -s {}
+if [ "$TODO" -eq "3" ] || [ "$TODO" -eq "all" ]; then
+    echo "Parsing ${THIRD_LIST}"
+    echo ${THIRD_LIST} | tr ' ' '\n' | xargs -n 1 -I {} -P ${PROCS} /myM5NR/bin/m5nr_compiler.py parse -d -f -s {}
+fi
 
 echo "Parsing Completed"
