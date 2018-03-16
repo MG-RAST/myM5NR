ca16a4bc073110865b73598f6e3f2e51fb57fb1a
Travis Harrison <guumby@gmail.com>
2018-03-16 10:34:56 -0500

    info

--- bin/download_PATRIC.sh
+++ bin/download_PATRIC.sh
@@ -4,6 +4,7 @@ GENOME_URL='https://p3.theseed.org/services/data_api/genome'
 FEATURE_URL='https://p3.theseed.org/services/data_api/genome_feature'
 
 # all between CDS count 0 - 10000, per range of 100
+TOTAL=0
 for MIN in `seq 0 100 9900`; do
     MAX=$((MIN + 100))
     GENOMES=`curl -s ${GENOME_URL} --data "eq(taxon_lineage_ids,131567)&select(genome_id)&limit(25000)&ge(patric_cds,${MIN})&lt(patric_cds,${MAX})" 2> /dev/null | tail -n +2 | tr -d '"'`
@@ -12,7 +13,7 @@ for MIN in `seq 0 100 9900`; do
         if [ ! -e ${FILE} ] ; then
             rm -f ${FILE}_part
             curl -s -o ${FILE}_part ${FEATURE_URL} -H 'Accept: application/solr+json' --data "in(genome_id,(${GID}))&limit(25000)&facet((field,feature_type),(mincount,1))" 2> /dev/null
-            mv ${FILE}_part ${FILE}
+            mv ${FILE}_part ${FILE} && TOTAL=$((TOTAL + 1))
         fi
     done
 done
@@ -24,6 +25,8 @@ for GID in `echo ${GENOMES}`; do
     if [ ! -e ${FILE} ] ; then
         rm -f ${FILE}_part
         curl -s -o ${FILE}_part ${FEATURE_URL} -H 'Accept: application/solr+json' --data "in(genome_id,(${GID}))&limit(25000)&facet((field,feature_type),(mincount,1))" 2> /dev/null
-        mv ${FILE}_part ${FILE}
+        mv ${FILE}_part ${FILE} && TOTAL=$((TOTAL + 1))
     fi
 done
+
+echo "downloaded ${TOTAL} genome feature files"
--- bin/m5nr_parse.sh
+++ bin/m5nr_parse.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-SOURCE=$1
+FIRST_LIST="BacMet CARD GenBank NCBI-Taxonomy SEED-Subsystems motuDB"
+SECOND_LIST="Greengenes PATRIC PhAnToMe RDP RefSeq SEED-Annotations SILVA-LSU SILVA-SSU Swiss-Prot TrEmble"
+THIRD_LIST="goslim CAZy COG EC EggNOG InterPro KEGG PFAM"
+
+PROCS=4
 
 cd /m5nr_data
-/myM5NR/bin/m5nr_compiler.py parse --debug --force --source $SOURCE
 
-while [ $? -eq 42 ]; do
-    echo "$SOURCE missing dependency, sleeping ..."
-    sleep 300
-    /myM5NR/bin/m5nr_compiler.py parse --debug --force --source $SOURCE
-done
+echo "Parsing ${FIRST_LIST}"
+echo ${FIRST_LIST} | tr ' ' '\n' | xargs -n 1 -I {} -P ${PROCS} /myM5NR/bin/m5nr_compiler.py parse -d -f -s {}
 
-echo "$SOURCE parsing completed"
+echo "Parsing ${SECOND_LIST}"
+echo ${SECOND_LIST} | tr ' ' '\n' | xargs -n 1 -I {} -P ${PROCS} /myM5NR/bin/m5nr_compiler.py parse -d -f -s {}
+
+echo "Parsing ${THIRD_LIST}"
+echo ${THIRD_LIST} | tr ' ' '\n' | xargs -n 1 -I {} -P ${PROCS} /myM5NR/bin/m5nr_compiler.py parse -d -f -s {}
+
+echo "Parsing Completed"
