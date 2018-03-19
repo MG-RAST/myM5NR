05c83f028076e5156f226a887046849684e8d743
Travis Harrison <guumby@gmail.com>
2018-03-19 14:08:22 -0500

    make parallel

--- bin/download_PATRIC.sh
+++ bin/download_PATRIC.sh
@@ -3,30 +3,47 @@
 GENOME_URL='https://p3.theseed.org/services/data_api/genome'
 FEATURE_URL='https://p3.theseed.org/services/data_api/genome_feature'
 
-# all between CDS count 0 - 10000, per range of 100
-TOTAL=0
-for MIN in `seq 0 100 9900`; do
-    MAX=$((MIN + 100))
-    GENOMES=`curl -s ${GENOME_URL} --data "eq(taxon_lineage_ids,131567)&select(genome_id)&limit(25000)&ge(patric_cds,${MIN})&lt(patric_cds,${MAX})" 2> /dev/null | tail -n +2 | tr -d '"'`
-    for GID in `echo ${GENOMES}`; do
-        FILE="${GID}.features.json"
-        if [ ! -e ${FILE} ] ; then
-            rm -f ${FILE}_part
-            curl -s -o ${FILE}_part ${FEATURE_URL} -H 'Accept: application/solr+json' --data "in(genome_id,(${GID}))&limit(25000)&facet((field,feature_type),(mincount,1))" 2> /dev/null
-            mv ${FILE}_part ${FILE} && TOTAL=$((TOTAL + 1))
-        fi
-    done
-done
+SELF=$0
+ACTION=$1
+VALUE=$2
 
-# all with CDS count > 10000
-GENOMES=`curl -s ${GENOME_URL} --data "eq(taxon_lineage_ids,131567)&select(genome_id)&limit(25000)&ge(patric_cds,10000)" 2> /dev/null | tail -n +2 | tr -d '"'`
-for GID in `echo ${GENOMES}`; do
-    FILE="${GID}.features.json"
-    if [ ! -e ${FILE} ] ; then
+# here we just download one genome file
+if [ "$ACTION" == "download" ]; then
+    
+    PREFIX=`echo ${VALUE} | cut -f1 -d'.'`
+    SIZE=${#PREFIX}
+    SUBDIR=""
+    if [ "$SIZE" -le "2" ]; then
+        SUBDIR=$PREFIX
+    else
+        SUBDIR=${PREFIX:0:2}
+    fi
+    
+    FILE="${SUBDIR}/${VALUE}.features.json"
+    if [ ! -e ${FILE} ]; then
         rm -f ${FILE}_part
         curl -s -o ${FILE}_part ${FEATURE_URL} -H 'Accept: application/solr+json' --data "in(genome_id,(${GID}))&limit(25000)&facet((field,feature_type),(mincount,1))" 2> /dev/null
-        mv ${FILE}_part ${FILE} && TOTAL=$((TOTAL + 1))
+        mv ${FILE}_part ${FILE}
     fi
-done
+    
+elif [ "$ACTION" == "wrapper" ]; then
+    
+    # all between CDS count 0 - 10000, per range of 100
+    for MIN in `seq 0 100 9900`; do
+        MAX=$((MIN + 100))
+        GENOMES=`curl -s ${GENOME_URL} --data "eq(taxon_lineage_ids,131567)&select(genome_id)&limit(25000)&ge(patric_cds,${MIN})&lt(patric_cds,${MAX})" 2> /dev/null | tail -n +2 | tr -d '"'`
+        COUNT=`echo ${GENOMES} | wc -l`
+        echo "Downloading ${COUNT} genome files, feature count range: ${MIN} - ${MAX}"
+        echo ${GENOMES} | xargs -n 1 -I {} -P ${VALUE} ${SELF} download {}
+    done
 
-echo "downloaded ${TOTAL} genome feature files"
+    # all with CDS count > 10000
+    GENOMES=`curl -s ${GENOME_URL} --data "eq(taxon_lineage_ids,131567)&select(genome_id)&limit(25000)&ge(patric_cds,10000)" 2> /dev/null | tail -n +2 | tr -d '"'`
+    COUNT=`echo ${GENOMES} | wc -l`
+    echo "Downloading ${COUNT} genome files, feature counts > 10000"
+    echo ${GENOMES} | xargs -n 1 -I {} -P ${VALUE} ${SELF} download {}
+
+else
+    echo "[error] invalid action request"
+    exit 1
+fi
