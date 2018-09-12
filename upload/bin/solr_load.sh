bcc4151c930361389d64e58cb5a5b3715860e24f
Travis Harrison <guumby@gmail.com>
2018-09-12 11:11:41 -0500

    solr collection creation

--- upload/bin/solr_load.sh
+++ upload/bin/solr_load.sh
@@ -8,12 +8,13 @@ IS_NODE=0
 INPUT_URI=''
 M5NR_VERSION=''
 SOLR_URL=''
+SOLR_CONTAINER='solr-m5nr'
 LOADER='/solr-import-export-json/run.sh'
 WORK_DIR='/m5nr_data/Upload'
 FILE_PATH=''
 
 function usage {
-  echo "Usage: $0 -i <input url> -v <m5nr version> -s <solr url> -l <loader cmd> -d <work dir> [-h -n]"
+  echo "Usage: $0 -i <input url> -v <m5nr version> -s <solr url> [-c <solr container> -l <loader cmd> -d <work dir> -h -n]"
   echo "   -h help message "
   echo "   -n input uri is shock node "
   echo "   -i input file path or shock url "
@@ -24,7 +25,7 @@ function usage {
 }
 
 # get options
-while getopts hni:v:s:l:d: option; do
+while getopts hni:v:s:c:l:d: option; do
     case "${option}"
 	in
 	    h) HELP=1;;
@@ -32,6 +33,7 @@ while getopts hni:v:s:l:d: option; do
 	    i) INPUT_URI=${OPTARG};;
         v) M5NR_VERSION=${OPTARG};;
         s) SOLR_URL=${OPTARG};;
+        c) SOLR_CONTAINER=${OPTARG};;
         l) LOADER=${OPTARG};;
         d) WORK_DIR=${OPTARG};;
     esac
@@ -59,7 +61,9 @@ echo "extracting ${FILE_PATH}"
 mkdir -p ${WORK_DIR}/solr_extract
 tar -zxf ${FILE_PATH} -C ${WORK_DIR}/solr_extract
 
-# create new collection for m5nr version
+# create new collection / config for m5nr version
+# must have docker socket mounted: -v "/var/run/docker.sock:/var/run/docker.sock"
+docker exec ${SOLR_CONTAINER} bash -c "cd /MG-RAST-infrastructure/ && git pull && cd services/solr-m5nr && ./setup-m5nr-core.sh ${M5NR_VERSION}"
 
 # load files
 for FILE in `ls ${WORK_DIR}/solr_extract`; do
