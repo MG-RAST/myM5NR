f4a15bf4611d9a27aee8a2251f0f358ef9077e11
Travis Harrison <guumby@gmail.com>
2018-10-02 11:00:02 -0500

    fix write

--- build/bin/tsv2csv.py
+++ build/bin/tsv2csv.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python -u
+
 import sys
 import csv
 
 tsvin  = csv.reader(sys.stdin, dialect=csv.excel_tab)
 csvout = csv.writer(sys.stdout, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 for row in tsvin:
-    csvout.write(row)
+    csvout.writerow(row)
